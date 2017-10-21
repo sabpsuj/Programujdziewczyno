942aae908d0eeae558a3db3ebc476a381624b35e
Sabina Psuj <sabinapsuj@gmail.com>
2017-10-21 17:17:15 +0200

    More - Less buttons

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
+/*global:document, console*/
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
     var forDropdown = document.querySelector('.for-dropdown');
     var dropDownMenu = document.querySelector('.dropdown');
-
-    console.log(forDropdown, dropDownMenu);
-    console.log(dropDownMenu.style);
     forDropdown.addEventListener('mouseover', function () {
         dropDownMenu.style.display = 'block';
     });
     forDropdown.addEventListener('mouseout', function () {
         dropDownMenu.style.display = 'none';
     });
+    var buttons = document.querySelectorAll('.read-more');
+    /*console.log(buttons[0].previousElementSibling);
+    buttons[0].addEventListener('click', */
+    function showHide() {
+        var textArea = this.previousElementSibling;
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+            textArea.style.display = 'block';
+        } else {
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+            textArea.style.display = 'none';
+        }
+    }
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
 });
