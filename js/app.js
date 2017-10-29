f6b749ecbccc4ce738fd614cd54b637bfaedcdac
Sabina Psuj <sabinapsuj@gmail.com>
2017-10-29 21:58:10 +0100

    Slider

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/*global:document, console*/
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
     var forDropdown = document.querySelector('.for-dropdown');
@@ -10,8 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         dropDownMenu.style.display = 'none';
     });
     var buttons = document.querySelectorAll('.read-more');
-    /*console.log(buttons[0].previousElementSibling);
-    buttons[0].addEventListener('click', */
+
     function showHide() {
         var textArea = this.previousElementSibling;
         if (textArea.style.display === 'none' || textArea.style.display === '') {
@@ -33,5 +31,55 @@ document.addEventListener('DOMContentLoaded', function () {
     var buttonBottom = document.querySelector('.btn-dark');
     buttonBottom.addEventListener('click', function () {
         window.scroll(0, 0);
+    });
+
+
+
+    var items = document.querySelectorAll('.org');
+    var dots = document.querySelectorAll('.dot');
+    console.log(items);
+    console.log(dots);
+
+
+// // for(var i=0; i < items.length; i++){
+// //     items[i].classList.remove("visible");
+// //     }
+
+// for(var i=0; i < dots.length; i++){
+//     dots[i].addEventListener('click', function(){
+//         dots[i].classList.remove("active");
+//         this.classList.add("active");
+//         // items[i].classList.remove("visible");
+//         console.log(this);
+//     })
+//     // console.log(this);
+//     // console.log(items.classList);
+// };
+// // console.log(dots);
+// // console.log(items);
+
+    dots[0].addEventListener('click', function(){
+        for(var i = 0; i < dots.length; i++){
+            dots[i].classList.remove("active");
+            items[i].classList.remove("visible");
+        }
+        this.classList.add("active");
+        items[0].classList.add("visible");
+    });
+    dots[1].addEventListener('click', function(){
+        for(var i = 0; i < dots.length; i++){
+            dots[i].classList.remove("active");
+            items[i].classList.remove("visible");
+        }
+        this.classList.add("active");
+        items[1].classList.add("visible");
+    });
+    dots[2].addEventListener('click', function(){
+        for(var i = 0; i < dots.length; i++){
+            dots[i].classList.remove("active");
+            items[i].classList.remove("visible");
+        }
+        this.classList.add("active");
+        items[2].classList.add("visible");
     });
 });
